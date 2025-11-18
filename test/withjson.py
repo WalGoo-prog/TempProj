011ba5a2e3cb63abae54a0ba6301cd88c302bf08
2023114626 홍예준 <walgooisgod@gmail.com>
2025-11-18 16:42:32 +0900

    집에보내다오

--- test/withjson.py
+++ test/withjson.py
@@ -2,19 +2,36 @@ import json
 
 
 class Data:
-    def __init__(self, chat, keywords, heritage):
-        _chat = chat
-        _keywords = keywords
-        _heritage = heritage
+    def __init__(self, chat = None, heritage = None):
+        self._chat = chat
+        self._heritage = heritage
 
+    def get_chat(self):
+        return self._chat
+
+    def get_heritage(self):
+        return self._heritage
+
+    def get_data(self):
+        res = dict()
+        res["chat"] = self._chat
+        res["heritage"] = self._heritage
+        return res
+
+    def set_heritage(self, heritage):
+        self._heritage = heritage
+
+    def set_chat(self, chat):
+        self._chat = chat
 
 def receive_resp():
     pass
 
+def data_to_dict(data: Data):
+    class_data = vars(data)
 
-def make_req():
+def make_req(data: Data):
     a = {
-        "keywords" : "임진왜란",
         "heritage" : "거북선",
         "chat" : {
             "id" : 1,
@@ -27,9 +44,16 @@ def make_req():
 
 def parsing():
     res = dict()
-
     return res
 
-test_data = Data("반가워요", "keywords", "거북선")
-res = make_req()
-print(res)
+file_path = 'GeoBukSeon.json'
+
+# 세이브 파일 안에 있는 특정 문화재의 데이터 파일 찾기
+try:
+    with open(file_path, 'r', encoding='utf-8') as f:
+        sample_data = json.load(f)
+except Exception as e:
+    print(e)
+
+not_used_keyword = sample_data["keywords"]["not_done"]
+
